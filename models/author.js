4315d888e8bee6fb1a1c97cf606ee9828ff846e7
elbezz <sofianesai78@gmail.com>
2021-10-10 22:47:46 +0100

    peoblem db fixed

--- models/author.js
+++ models/author.js
@@ -1,8 +1,8 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 const authorSchema = new mongoose.Schema({
-    name:{
-        type:String,
-        required:true
-    }
-})
-module.exports = mongoose.model('Author', authorSchema)
+  name: {
+    type: String,
+    required: true,
+  },
+});
+module.exports = mongoose.model("Author", authorSchema);
--- models/book.js
+++ models/book.js
@@ -24,9 +24,9 @@ const bookSchema = new mongoose.Schema({
     type: Buffer,
     required: true,
   },
-  coverImageType:{
-    type:String,
-    required:true
+  coverImageType: {
+    type: String,
+    required: true,
   },
   author: {
     type: mongoose.Schema.Types.ObjectId,
@@ -36,8 +36,10 @@ const bookSchema = new mongoose.Schema({
 });
 
 bookSchema.virtual("coverImagePath").get(function () {
-  if (this.coverImage != null && this.coverImageType !=null) {
-  return `data:${this.coverImageType}; charset=utf-8;base64,${this.coverImage.toString('base64')}`
+  if (this.coverImage != null && this.coverImageType != null) {
+    return `data:${
+      this.coverImageType
+    }; charset=utf-8;base64,${this.coverImage.toString("base64")}`;
   }
 });
 module.exports = mongoose.model("Book", bookSchema);
